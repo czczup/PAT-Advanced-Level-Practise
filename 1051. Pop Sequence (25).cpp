85efda95e9f1a894f5d48a13b9b89c171ee4f4c7
bibibi <wztxy89@163.com>
2017-09-23 15:34:03 +0800

    栈模拟

--- 1051. Pop Sequence (25).cpp
+++ 1051. Pop Sequence (25).cpp
@@ -1,10 +1,11 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stack>
 using namespace std;
 int main(){
 	int M,N,K; 
 	scanf("%d %d %d",&M,&N,&K);
 	for(int i=0;i<K;i++){
+		/*读入一组出栈顺序*/
 	    stack<int> s;
 	    int num[N];
 	    for(int j=0;j<N;j++){
@@ -12,6 +13,7 @@ int main(){
 		}
 	    int current = 0;
 		bool flag = false;
+		/*开始模拟出栈*/
 	    for(int j=1;j<=N;j++){
 	        s.push(j);
 	        if(s.size()>M)
@@ -21,6 +23,7 @@ int main(){
 	            current++;
 			}
 		}
+		/*格式化输出*/
 	    if(current == N)
 	        flag = true;
 	    if(flag == true)
