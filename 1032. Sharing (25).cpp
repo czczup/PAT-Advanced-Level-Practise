177eaead76bacc498c202d13c5f6dbd1e3072cb0
bibibi <wztxy89@163.com>
2017-10-15 13:47:02 +0800

    链表

---
+++ 1032. Sharing (25).cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+typedef struct Node{
+	int flag;
+	char data;
+	int next;
+}Node;
+
+int main(){
+	int string1,string2,num;
+	scanf("%d %d %d",&string1,&string2,&num);
+	Node n[100000];
+	for(int i=0;i<num;i++){
+		int address,next;
+		char data;
+		scanf("%d %c %d",&address,&data,&next);
+		n[address].flag = 0;     //0表示未被访问 
+		n[address].data = data;
+		n[address].next = next;
+	}
+	/*遍历第1条链表,访问过的结点标记为1*/
+	while(string1!=-1){
+		n[string1].flag = 1;
+		string1 = n[string1].next;
+	}
+	/*遍历第2条链表，找到第1个被标记为1的结点*/
+	while(string2!=-1){
+		if(n[string2].flag==1){
+			printf("%05d",string2);
+			return 0;
+		}
+		string2 = n[string2].next;
+	}
+	printf("-1");
+	return 0;
+}
+
